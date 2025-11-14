ce21ada3a1eba1bede306008556424dbfe3e28b9
Alessandro Argenziano <1603alessandro@gmail.com>
2025-11-14 18:41:36 +0100

    fix: track environment files for angular production build

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  apiBase: '',
+  firebase: {
+    apiKey: 'AIzaSyBxxdRSUNHCx6z_cWZAg8wb-wtQAMWMqwk',
+    authDomain: 'chess-401ca.firebaseapp.com',
+    databaseURL:
+      'https://chess-401ca-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'chess-401ca',
+    storageBucket: 'chess-401ca.firebasestorage.app',
+    messagingSenderId: '92362330433',
+    appId: '1:92362330433:web:3cf1f594249f03d60c51c0',
+  },
+};
